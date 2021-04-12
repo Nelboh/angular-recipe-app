0a1f80df4d4b037b430d94064b8e2910c50dd24b
Eleanor Hobbs <eleanor.hobbs@activequote.com>
2021-04-12 10:39:23 +0100

    Ingredients can now be added to shopping list with add button

--- recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,23 @@
-export class ShoppingListService {
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model'; 
 
+export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Tomatoes', 10),
+        new Ingredient ('Breadsticks', 62),
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice(); 
+        // ...makes a copy so original can't be modified
+        // removing slice would let you add ingredients to the original list
+        
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
